99410dd5ec0236b5f9a832d409b03f70b92c3c54
Ricardo <114017873+Ranz00@users.noreply.github.com>
2025-11-26 14:32:28 -0300

    Delete src/main/java/com/example/userapi/service/UserService.java

--- src/main/java/com/example/userapi/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.userapi.dto;
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-    @NotBlank String usernameOrEmail,
-    @NotBlank String password
-) {}
